b827b1d6e08fa700045378d70402735b7db6ac56
Victor Bellu <v.c.bellu@gmail.com>
2019-08-22 11:33:35 +0200

    work plsease

---
+++ Half_pyramidbuilder.py
@@ -0,0 +1,11 @@
+
+# a funktion that builds a half pyramid out of "#" as high as you want it
+
+
+
+
+def pyramidbuilder(height):
+        for x in range(2,height + 2):
+            print(x * "#")
+
+pyramidbuilder(5)
